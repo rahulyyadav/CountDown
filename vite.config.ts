ae8639b886d4e9c21ae5210c55b20448d3ef38c6
Rahul Yadav <rahulyyadav21@gmail.com>
2025-01-15 22:57:14 +0530

    changed vite confg

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // Use '/' if deploying to the root domain; adjust if deploying to a subfolder
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
